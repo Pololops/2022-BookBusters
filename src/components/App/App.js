738b83970eaea3bd29bc162dca45c593df52c2d9
[Pierre-Nicolas] <[pnpvdc@gmail.com]>
2022-03-30 19:40:14 +0200

    First react components and test au Material Design

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,33 @@
 import './style.scss';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Stack spacing={2} direction="row">
+          <Button
+            variant="contained"
+            color="primary"
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </Button>
+        </Stack>
       </header>
     </div>
   );
