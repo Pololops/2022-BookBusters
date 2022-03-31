58589dc8c6250270065fbefefca52fedc63db11f
Pololops <paul.rigaudeau@me.com>
2022-03-31 20:55:12 +0200

    add route to get cover and cover to book found by ISBN

--- app/services/openLibrary.js
+++ app/services/openLibrary.js
@@ -1,3 +1,5 @@
+const debug = require('debug')('openLibraryAPI')
+
 const fetch = require('node-fetch');
 
 /**
@@ -12,28 +14,35 @@ const openLibrary = {
             const response = await fetch(url);
             const json = await response.json();
 
+            debug(json);
+
             let result = {};
 
             // If no answer
-            if (json.totalItems == 0) {
+            if (Object.entries(json).length === 0) {
                 result = undefined;
             }
 
             // If at least one answer
-            if (json.totalItems >= 1) {
+            if (Object.entries(json).length >= 1) {
+                debug('1 resultat');
                 const ISBNKey = `ISBN:${isbn}`;
 
+                debug(ISBNKey);
+
                 // If no cover found in result
-                if (!(json.items[0][ISBNKey].thumbnail_url)) {
+                if (!(json[ISBNKey].thumbnail_url)) {
+                    debug('pas de cover trouvée dans le résultat');
                     result = undefined;
                 } else {
+                    debug('cover trouvée dans le résultat');
                     result = {
-                        coverM: json.items[0][ISBNKey].thumbnail_url.split('-S').join('-M'),
-                        coverL: json.items[0][ISBNKey].thumbnail_url.split('-S').join('-L'),
+                        coverM: json[ISBNKey].thumbnail_url.split('-S').join('-M'),
+                        coverL: json[ISBNKey].thumbnail_url.split('-S').join('-L'),
                     };
                 }
             }
-            
+            debug(result);
             return result;
         } catch (error) {
             throw error;
