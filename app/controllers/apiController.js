5bba4cfbff82e6dbe820e15a0eb634cd8f4cfa1d
Pololops <paul.rigaudeau@me.com>
2022-03-31 20:12:35 +0200

    Code OK, in testing

--- app/controllers/apiController.js
+++ app/controllers/apiController.js
@@ -4,9 +4,17 @@ const ApiError = require('../errors/apiError');
 module.exports = {
     async getBookByISBN(req, res) {
         const book = await google.findBookByISBN(req.params.isbn);
-        if (!book){
+        if (!book) {
             throw new ApiError('Sorry, book with this ISBN not found', 404);
         }
         return res.json(book);
-    }
+    },
+
+    async getBookCoverByISBN(req, res) {
+        const cover = await openLibrary.findBookCoverByISBN(req.params.isbn);
+        if (!cover) {
+            throw new ApiError('Sorry, book cover with this ISBN not found', 404);
+        }
+        return res.json(book);
+    },
 };
