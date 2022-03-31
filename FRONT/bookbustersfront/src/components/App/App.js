497087dc89206c7b3b730153c38b0001e3d01419
[Pierre-Nicolas] <[pnpvdc@gmail.com]>
2022-03-31 13:53:05 +0200

    Components folders

--- FRONT/bookbustersfront/src/components/App/App.js
+++ FRONT/bookbustersfront/src/components/App/App.js
@@ -1,34 +1,8 @@
 import './style.scss';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Stack spacing={2} direction="row">
-          <Button
-            variant="contained"
-            color="primary"
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </Button>
-        </Stack>
-      </header>
     </div>
   );
 }
