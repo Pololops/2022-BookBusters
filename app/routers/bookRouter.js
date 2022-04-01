d4059578c339e0af6333ddee69b76a140bcff775
YvanMackowiak <yvanmackowiak@gmail.com>
2022-04-01 14:36:57 +0200

    Routes base

--- app/routers/bookRouter.js
+++ app/routers/bookRouter.js
@@ -3,24 +3,35 @@ const express = require('express');
 const router = express.Router();
 
 const controllerHandler = require('../middlewares/controllerWrapperAsync');
-
 const bookController = require('../controllers/bookController');
+
+const validate = require('../validator/validator');
+const createSchema = require('../validator/schemas/bookCreate');
 
 router
     .route('/book')
     /**
      * GET /v1/book
-     * @summary Get all books
-     * @tags BOOKS
+     * @summary Get all books in donation with users, order by last one
+     * @tags BOOK
      * @return {[Book]} 200 - success response - application/json
      */
-    .get(controllerHandler(bookController.getAll));
+    .get(controllerHandler(bookController.getAllInDonation))
+    /**
+     * POST /v1/book
+     * @summary Add book to database
+     * @tags BOOK
+     * @param {InputBook} request.body.required - book info
+     * @return {Book} 200 - success response - application/json
+     * @return {ApiError} 400 - Bad request response - application/json
+     */
+    .post(validate(createSchema, 'body'), controllerHandler(bookController.addBook));
 
 router
-    .route('/book/:id')
+    .route('/book/:id(\\d+)')
     /**
      * GET /v1/book/{id}
-     * @summary Get one book by id
+     * @summary Get one book in donation by id
      * @param {number} id.path.required - book identifier
      * @tags BOOK
      * @return {Book} 200 - success response - application/json
