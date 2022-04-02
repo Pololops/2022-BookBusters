fbd78f19bb40b43287f3f390dd4a45951466c342
YvanMackowiak <yvanmackowiak@gmail.com>
2022-04-03 01:10:38 +0200

    header creates mobile and pc version... good night

--- FRONT/bookbustersfront/src/components/BurgerMenu/BurgerMenu.js
+++ FRONT/bookbustersfront/src/components/BurgerMenu/BurgerMenu.js
@@ -1,44 +1,44 @@
 import { NavLink } from "react-router-dom";
+import { useState } from 'react';
 
 const BurgerMenu = () => {
+
+    const [showLinks, setShowLinks] = useState(false)
+
+    const handleShowLinks = () => {
+        setShowLinks(!showLinks)
+    }
+
     return (
-        <div>
-            <nav>
-                <div className="navbar">
-                    <div className="container nav-container">
-                        <input className="checkbox" type="checkbox" name="" id="" />
-                        <div className="hamburger-lines">
-                            <span className="line line1"></span>
-                            <span className="line line2"></span>
-                            <span className="line line3"></span>
-                        </div>  
-                        <div className="menu-items">
-                            <NavLink to="/Library">
-                                <li>Ma bibliothèque</li>
-                            </NavLink>
-                            <NavLink to="/Favorites">
-                                <li>Mes favoris</li>
-                            </NavLink>
-                            <NavLink to="/">
-                                <li>Mes alertes</li>
-                            </NavLink>
-                            <NavLink to="/Account">
-                                <li>Mon compte</li>
-                            </NavLink>
-                            <NavLink to="/Contact">
-                                <li>Contact</li>
-                            </NavLink>
-                            <NavLink to="/Credits">
-                                <li>Crédits</li>
-                            </NavLink>
-                            <NavLink to="/LegalNotice">
-                                <li>Mentions légales</li>
-                            </NavLink>                            
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        </div>
+        <nav className={`navbar ${showLinks ? "show-nav" : "hide-nav"}`}>
+            <div className="navbar_logo">BookBusters</div>
+            <ul className='navbar_links'>
+                <NavLink to="/Library">
+                    <li>Ma bibliothèque</li>
+                </NavLink>
+                <NavLink to="/Favorites">
+                    <li>Mes favoris</li>
+                </NavLink>
+                <NavLink to="/">
+                    <li>Mes alertes</li>
+                </NavLink>
+                <NavLink to="/Account">
+                    <li>Mon compte</li>
+                </NavLink>
+                <NavLink to="/Contact">
+                    <li>Contact</li>
+                </NavLink>
+                <NavLink to="/Credits">
+                    <li>Crédits</li>
+                </NavLink>
+                <NavLink to="/LegalNotice">
+                    <li>Mentions légales</li>
+                </NavLink>               
+            </ul>                   
+            <button className='navbar_burger' onClick={handleShowLinks}>
+                <span className='burger-bar'></span>
+            </button>       
+        </nav>
     );
 };
 
