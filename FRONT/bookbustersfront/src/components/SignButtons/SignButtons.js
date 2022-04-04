16836131bc805aa61245cbce711a1cab716aa4d0
Pierre-Nicolas <pnpvdc@gmail.com>
2022-04-04 15:03:16 +0200

    MUI Template

--- FRONT/bookbustersfront/src/components/SignButtons/SignButtons.js
+++ FRONT/bookbustersfront/src/components/SignButtons/SignButtons.js
@@ -1,14 +1,6 @@
 import { Stack, Button, ButtonGroup } from '@mui/material'
 
-// import { createTheme, ThemeProvider } from '@mui/system'
 import React from 'react'
-
-// const themeSignButtons = createTheme({
-//     text: {
-//         primary: '#2e7d32',
-//         secondary:'#ff8f00'
-//     },
-// });
 
 function SignButtons() {
   return (
