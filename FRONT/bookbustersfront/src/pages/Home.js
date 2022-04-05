615f8a1fe9c72040d9388942678da0121aa94fbc
Pierre-Nicolas <pnpvdc@gmail.com>
2022-04-05 10:18:01 +0200

    Refactor routing and SignIn SignUp

--- FRONT/bookbustersfront/src/pages/Home.js
+++ FRONT/bookbustersfront/src/pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
             <Header />
 
             <div className="text-container">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur accumsan tellus vitae ligula ultricies placerat. Vivamus mattis nulla lacus, vitae commodo massa blandit ut. Pellentesque lobortis velit nisi, at congue nibh ultrices quis. Etiam nec nunc ac ipsum sodales condimentum vel a urna. In et nisl sed purus semper pharetra. Duis vehicula sapien elit, eu auctor tortor rhoncus finibus. Vestibulum vestibulum tincidunt varius. Morbi nec mattis elit, at congue felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                     ipsum dolor sit amet, consectetur adipiscing elit. Curabitur accumsan tellus vitae ligula ultricies placerat. Vivamus mattis nulla lacus, vitae commodo massa blandit ut. Pellentesque lobortis velit nisi, at congue nibh ultrices quis. Etiam nec nunc ac ipsum sodales condimentum vel a urna. In et nisl sed purus semper pharetra. Duis vehicula sapien elit, eu auctor tortor rhoncus finibus. Vestibulum vestibulum tincidunt varius. Morbi nec mattis elit, at congue felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             </div>
 
             <SignButtons />
