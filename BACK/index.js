ca2d4ebeb4d06b9f175854a63bb1912dd127a907
Pololops <paul.rigaudeau@me.com>
2022-04-05 17:27:33 +0200

    config for SSL server

--- BACK/index.js
+++ BACK/index.js
@@ -1,8 +1,22 @@
 require('dotenv').config();
+
+const debug = require('debug')('server');
+
+const fs = require('fs');
+const https = require('https');
+
 const app = require('./app/app');
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`);
-});
+https
+    .createServer(
+        {
+            key: fs.readFileSync('conf/server.key'),
+            cert: fs.readFileSync('conf/server.cert'),
+        },
+        app,
+    )
+    .listen(port, () => {
+        debug(`Server started on https://localhost:${port}`);
+    });
