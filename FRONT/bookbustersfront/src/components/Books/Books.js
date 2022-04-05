14bba704bdadd705bee1d77c37865e565c9c9d51
Pierre-Nicolas <pnpvdc@gmail.com>
2022-04-05 18:54:05 +0200

    No errors, all actual components OK

--- FRONT/bookbustersfront/src/components/Books/Books.js
+++ FRONT/bookbustersfront/src/components/Books/Books.js
@@ -1,32 +1,42 @@
-import { Button, ButtonGroup, Stack } from '@mui/material'
-import { Box } from '@mui/system'
-import React from 'react'
-import Book from '../Book/Book'
-
+import { Button, ButtonGroup, Container, Stack } from '@mui/material';
+import { Box } from '@mui/system';
+import React from 'react';
+import Book from '../Book/Book';
 
 // C'est ici que nous allons faire notre map pour afficher plusieurs livres.
 
 function Books() {
-  return (
-      <>
-      <Stack direction='row'>
-        <ButtonGroup>
-            <Button variant='contained' color='primary'>Les derniers</Button>
-            <Button variant='contained' color='secondary'>Autour de moi</Button>
-        </ButtonGroup>
-      </Stack>
-    <Box sx={{
-        display:'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-evenly'
-    }} >
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-    </Box>
-    </>
-  )
+    return (
+        <>
+            <Container
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                }}
+            >
+                <ButtonGroup>
+                    <Button variant="contained" color="primary">
+                        Les derniers
+                    </Button>
+                    <Button variant="contained" color="secondary">
+                        Autour de moi
+                    </Button>
+                </ButtonGroup>
+            </Container>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    justifyContent: 'space-evenly',
+                }}
+            >
+                <Book />
+                <Book />
+                <Book />
+                <Book />
+            </Box>
+        </>
+    );
 }
 
-export default Books
+export default Books;
