dc0e0dc4f45ab86daa39c56d952fd566c9983ef5
ElodieFaivre <elodie.faivre3@gmail.com>
2022-04-05 10:52:13 +0200

    insertOrUpdateBook method finished to add book in library, favorite or alert

--- app/models/book.js
+++ app/models/book.js
@@ -1,4 +1,5 @@
 const client = require('../config/database');
+const ApiError = require('../errors/apiError');
 const debug = require('debug')('BookController');
 /**
  * @typedef {object} User
@@ -45,73 +46,123 @@ const bookDataMapper = {
         return result.rows[0];
     },
 
+    async insert(book) {
+        const result = await client.query(
+            ` INSERT INTO book
+            (isbn13, isbn10) VALUES
+            ($1, $2) RETURNING *
+        `,
+            [book.isbn13, book.isbn10],
+        );
+        return result.rows[0];
+    },
+
     /**
      * Ajoute ou modifier dans la base de données
      * @param {InputBook} book - Les données à insérer
      * @returns {Book} - Le livre ajouté
      */
     async updateOrInsert(book) {
+
+        //0. Check input body and update default values :
+        //  - If is_in_library= TRUE, is_in_donation= TRUE.
+        //  - If is_in_donation = TRUE, is_in_library = TRUE
+        //  - If is_in_library= FALSE, is_in_donation= FALSE
+        //  - If is_in_alert = TRUE, check if is_in_library = TRUE and error if it is (see later)
+        if (book.is_in_library) { book.is_in_donation = true; }
+        if (book.is_in_donation) { book.is_in_library = true; }
+        if (!book.is_in_library) { book.is_in_donation = false; }
+
+
+
         //1. Verify if this book exist in our BDD
-        let bookId = await client.query(
+        let bookExist = await client.query(
             `SELECT id FROM book WHERE isbn13=$1 OR isbn10=$2`,
             [book.isbn13, book.isbn10],
         );
 
-
-        //TODO : user id dynamic
-        let userBookRelation=null;
-        let userBook=null;
+        //TODO : user id par le token ? Pour le moment dans le body post.
+        let bookId = null;
+        let userBookRelation = null;
+        let userBook = null;
         // 2. If book exist, verify if relation exist
-        if(bookId.rowCount>0){
-            debug('le livre existe')
-            bookId=bookId.rows[0].id;
+        if (bookExist.rowCount > 0) {
+            bookId = bookExist.rows[0].id;
             debug('le livre existe avec id', bookId)
-            userBookRelation =await client.query(
+            userBookRelation = await client.query(
                 `SELECT * FROM user_has_book WHERE book_id=$1 AND user_id=$2`,
                 [bookId, book.user_id],
             );
         }
 
-        //2. If book does not exist yet, add book
-       else {
+        //3. If book does not exist yet, add book
+        else {
             debug('le livre n existe pas, je vais le créer')
-            bookId = await client.query(
-                ` INSERT INTO book
-                (isbn13, isbn10) VALUES
-                ($1, $2) RETURNING id
-            `,
-                [book.isbn13, book.isbn10],
-            );
-            bookId=bookId.rows[0].id;
+            const bookInserted = await this.insert(book)
+            bookId = bookInserted.id;
             debug('livre créé avec id', bookId);
         }
-        //3. If relation exist, update
-        if(userBookRelation && userBookRelation.rowCount>0){
+
+        //4. If relation exist, update
+        if (userBookRelation && userBookRelation.rowCount > 0) {
             debug('la relation de ce livre avec user existe déjà, update')
             debug("user id", userBookRelation.rows[0].id)
-            userBook = await client.query(
-                ` UPDATE user_has_book SET
-                "is_in_library"=$1, "is_in_donation"=$2, "is_in_favorite"=$3, "is_in_alert"=$4 WHERE id=$5 RETURNING *
-            `,
-                [book.is_in_library, book.is_in_donation, book.is_in_favorite, book.is_in_alert, userBookRelation.rows[0].id],
-            );
+            //Check
+            //  - If is_in_alert = TRUE, check if is_in_library = TRUE and error if it is (see later)
+            if(book.is_in_alert){
+                if(userBookRelation.rows[0].is_in_library){
+                    throw new ApiError('Book already in library cannot be in alert', 400)
+                }
+            }
+            //if book_in_donation, update donation_date
+            if(book.is_in_donation){
+                userBook = await client.query(
+                    ` UPDATE user_has_book SET
+                    "is_in_library"=$1, "is_in_donation"=$2, "is_in_favorite"=$3, "is_in_alert"=$4, "donation_date"=NOW() WHERE id=$5 RETURNING *
+                `,
+                    [book.is_in_library, book.is_in_donation, book.is_in_favorite, book.is_in_alert, userBookRelation.rows[0].id],
+                );
+            }
+            else{
+                userBook = await client.query(
+                    ` UPDATE user_has_book SET
+                    "is_in_library"=$1, "is_in_donation"=$2, "is_in_favorite"=$3, "is_in_alert"=$4 WHERE id=$5 RETURNING *
+                `,
+                    [book.is_in_library, book.is_in_donation, book.is_in_favorite, book.is_in_alert, userBookRelation.rows[0].id],
+                );
+            }
+
             debug('update terminé')
         }
-        //4 else create
+
+        //5. else create
         else {
-            debug('la relation de ce livre avec user existe pas, je créé')
+            debug('la relation de ce livre avec user n existe pas, je créé')
             debug('bookId', bookId)
-           userBook = await client.query(
-                ` INSERT INTO user_has_book
-                (book_id, user_id, is_in_library, is_in_donation, is_in_favorite, is_in_alert) VALUES
-                ($1, $2, $3, $4, $5, $6) RETURNING *
-            `,
-                [bookId, book.user_id, book.is_in_library, book.is_in_donation, book.is_in_favorite, book.is_in_alert],
-            );
+            if(book.is_in_donation){
+                userBook = await client.query(
+                    ` INSERT INTO user_has_book
+                    (book_id, user_id, is_in_library, is_in_donation, is_in_favorite, is_in_alert, donation_date) VALUES
+                    ($1, $2, $3, $4, $5, $6, NOW()) RETURNING *
+                `,
+                    [bookId, book.user_id, book.is_in_library, book.is_in_donation, book.is_in_favorite, book.is_in_alert],
+                );
+            }
+            else {
+                userBook = await client.query(
+                    ` INSERT INTO user_has_book
+                    (book_id, user_id, is_in_library, is_in_donation, is_in_favorite, is_in_alert) VALUES
+                    ($1, $2, $3, $4, $5, $6) RETURNING *
+                `,
+                    [bookId, book.user_id, book.is_in_library, book.is_in_donation, book.is_in_favorite, book.is_in_alert],
+                );
+            }
+
             debug('creation terminé')
         }
         debug('livre user-book MAJ', userBook.rows)
-        //6 Then return book with status
+
+        //6. Then return book with status
         return userBook.rows[0];
     },
 };
