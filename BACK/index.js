ae63ac64ec8037b9dcf8c9bc2db63a82b9fea14e
Pololops <paul.rigaudeau@me.com>
2022-04-05 17:30:00 +0200

    change console.log into debug

--- BACK/index.js
+++ BACK/index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
+
+const debug = require('debug')('Server');
 const app = require('./app/app');
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`);
+    debug(`Server started on http://localhost:${port}`);
 });
