7953770796e1b57d975e9923dcfa79ca1f800521
JulienDupont11 <dupont.julien@gmail.com>
2022-04-05 12:00:45 +0200

    beginning of the connection to the worldcat API

---
+++ app/services/worldCat.js
@@ -0,0 +1,53 @@
+//const debug = require('debug')('openLibraryAPI')
+
+const fetch = require('node-fetch');
+
+/**
+ * @typedef {object} BookCover
+ * @property {string} cover - Book cover url
+ */
+
+const worldCat = {
+    async findBookCoverByISBN(isbn) {
+        try {
+            const url = `http://classify.oclc.org/classify2/Classify?isbn=${isbn}&summary=true`;
+            const response = await fetch(url);
+            const json = await response.json();
+
+            debug(json);
+
+            let result = {};
+
+            // If no answer
+            if (Object.entries(json).length === 0) {
+                result = undefined;
+            }
+
+            // If at least one answer
+            if (Object.entries(json).length >= 1) {
+                debug('1 resultat');
+                const ISBNKey = `ISBN:${isbn}`;
+
+                debug(ISBNKey);
+
+                // If no cover found in result
+                if (!(json[ISBNKey].thumbnail_url)) {
+                    debug('pas de cover trouvée dans le résultat');
+                    result = undefined;
+                } else {
+                    debug('cover trouvée dans le résultat');
+                    result = {
+                        coverM: json[ISBNKey].thumbnail_url.split('-S').join('-M'),
+                        coverL: json[ISBNKey].thumbnail_url.split('-S').join('-L'),
+                    };
+                }
+            }
+            debug(result);
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    },
+};
+
+module.exports = worldCat;
