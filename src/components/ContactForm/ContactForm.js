e47c070624751e59331233e72dff5bc4eac8e9e3
Pierre-Nicolas <pnpvdc@gmail.com>
2022-04-05 14:56:47 +0200

    Book Cards

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,9 +1,59 @@
 import React from 'react'
 import { Button, TextField } from "@mui/material";
+import { useFormControls } from './FormControls';
+
+const inputFieldValues = [
+    {
+      name: "fullName",
+      label: "Full Name",
+      id: "my-name"
+    },
+    {
+      name: "email",
+      label: "Email",
+      id: "my-email"
+    },
+    {
+      name: "message",
+      label: "Message",
+      id: "my-message",
+      multiline: true,
+      rows: 10
+    }
+  ];
 
 function ContactForm() {
+    const {
+        handleInputValue,
+        handleFormSubmit,
+        formIsValid,
+        errors
+      } = useFormControls();
   return (
-    <div>ContactForm</div>
+    <form onSubmit={handleFormSubmit}>
+      {inputFieldValues.map((inputFieldValue, index) => {
+        return (
+          <TextField
+            key={index}
+            onBlur={handleInputValue}
+        onChange={handleInputValue}
+            name={inputFieldValue.name}
+            label={inputFieldValue.label}
+            multiline={inputFieldValue.multiline ?? false}
+            rows={inputFieldValue.rows ?? 1}
+        autoComplete="none"
+        {...(errors[inputFieldValue.name] && { error: true, helperText: errors[inputFieldValue.name] })}
+
+          />
+        );
+      })}
+      <Button
+        type="submit"
+        disabled={!formIsValid()}
+      >
+        Send Message
+      </Button>
+    </form>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,16 @@
-import { Typography } from "@mui/material";
+import * as React from 'react';
+
 import Header from "../components/Header/Header";
 import SignButtons from "../components/SignButtons/SignButtons";
+import Book from '../components/Book/Book';
+
 import '../styles/Home.scss'
 
 const Home = () => {
     return (
         <>
             <Header />
-            <Typography variant="body1">
-            Bon alors en garde, fils d’unijambiste. Alors dites vous que c’est un combat réel
-            et montrez-moi ce que vous avez dans l’slibard! Ben évidemment que c’est vrai! Vous
-            avez jamais dit que ça devait être faux! Ah ah Sire! Je vous attends! À moins que vous
-            préfériez qu’on dise partout que le roi est une petite pédale qui pisse dans son froc à l’idée d’se battre!
-            </Typography>
+            <Book />
             <SignButtons />
         </>
     );
