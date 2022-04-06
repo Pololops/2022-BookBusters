69452152361b13310ea26b8630c995ee3e4b3ff1
Pierre-Nicolas <pnpvdc@gmail.com>
2022-04-06 18:45:06 +0200

    SearchBar

--- FRONT/bookbustersfront/src/components/AppBar/AppBarBB.js
+++ FRONT/bookbustersfront/src/components/AppBar/AppBarBB.js
@@ -1,82 +1,89 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container'
-import MenuBookIcon from '@mui/icons-material/MenuBook';
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
 import { Link } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
 
 const pages = [
-    {
-        href: '/',
-        style: {textDecoration: 'none' },
-        sx: { color: { xs: 'black', md: 'white' } },
-        label: 'Accueil',
+  {
+    href: "/",
+    style: { textDecoration: "none" },
+    sx: { color: { xs: "black", md: "white" } },
+    label: "Accueil",
+  },
+  {
+    href: "/Contact",
+    style: { textDecoration: "none" },
+    sx: { color: { xs: "black", md: "white" } },
+    label: "Contact",
+  },
+  {
+    href: "/Credits",
+    style: { textDecoration: "none" },
+    sx: { color: { xs: "black", md: "white" } },
+    label: "Crédits",
+  },
+  {
+    href: "/LegalNotice",
+    style: { textDecoration: "none" },
+    sx: { color: { xs: "black", md: "white" } },
+    label: "Mentions légales",
+  },
+  {
+    href: "/signUp",
+    style: { textDecoration: "none" },
+    sx: {
+      color: { xs: "black", md: "white" },
+      display: { xs: "none", md: "block" },
     },
-    {
-        href: '/Contact',
-        style: {textDecoration: 'none' },
-        sx: { color: { xs: 'black', md: 'white' } },
-        label: 'Contact',
+    label: "Inscription",
+  },
+  {
+    href: "/SignIn",
+    style: { textDecoration: "none", color: "fff" },
+    sx: {
+      color: { xs: "black", md: "white" },
+      display: { xs: "none", md: "block" },
     },
-    {
-        href: '/Credits',
-        style: { textDecoration: 'none' },
-        sx: { color: { xs: 'black', md: 'white' } },
-        label: 'Crédits',
-    },
-    {
-        href: '/LegalNotice',
-        style: { textDecoration: 'none' },
-        sx: { color: { xs: 'black', md: 'white' } },
-        label: 'Mentions légales',
-    },
-    {
-        href: '/signUp',
-        style: { textDecoration: 'none' },
-        sx: { color: { xs: 'black', md: 'white' }, display:{ xs:"none" ,md:"block"} },
-        label: 'Inscription',
-    },
-    {
-        href: '/SignIn',
-        style: { textDecoration: 'none' ,color:"fff"},
-        sx: { color: { xs: 'black', md: 'white' }, display:{ xs:"none" ,md:"block"} },
-        label: 'Connexion',
-    },
+    label: "Connexion",
+  },
 ];
 
 const settings = [
-    {
-        href: '/Account',
-        style: {color:"#fff", textDecoration: 'none'},
-        sx: { color:'black'},
-        label: 'Account',
-    },
-    {
-        href: '/MyAlerts',
-        style: {color:"#fff", textDecoration: 'none'},
-        sx:  { color:'black'},
-        label: 'Mes alertes',
-    },
-    {
-        href: '/Library',
-        style: {color:"#fff", textDecoration: 'none'},
-        sx:  { color:'black'},
-        label: 'Ma bibliothèque',
-    },
-    {
-        href: '/Favorites',
-        style: {color:"#fff", textDecoration: 'none'},
-        sx:  { color:'black'},
-        label: 'Mes favoris',
-    },
+  {
+    href: "/Account",
+    style: { color: "#fff", textDecoration: "none" },
+    sx: { color: "black" },
+    label: "Account",
+  },
+  {
+    href: "/MyAlerts",
+    style: { color: "#fff", textDecoration: "none" },
+    sx: { color: "black" },
+    label: "Mes alertes",
+  },
+  {
+    href: "/Library",
+    style: { color: "#fff", textDecoration: "none" },
+    sx: { color: "black" },
+    label: "Ma bibliothèque",
+  },
+  {
+    href: "/Favorites",
+    style: { color: "#fff", textDecoration: "none" },
+    sx: { color: "black" },
+    label: "Mes favoris",
+  },
 ];
 
 const ResponsiveAppBar = () => {
@@ -99,23 +106,23 @@ const ResponsiveAppBar = () => {
   };
 
   return (
-    <AppBar position="sticky" sx={{top:0, left:0, right:0}}>
-
+    <AppBar position="sticky" sx={{ top: 0, left: 0, right: 0 }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-            <Typography
-                variant="h6"
-                noWrap
-                component="div"
-                sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
-            >
-            <MenuBookIcon sx={{marginRight: '10px'}}/>
-              <Link to="/" style={{color:"#fff", textDecoration: 'none'}}>
-                BookBusters
-              </Link>
-            </Typography>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+          >
+            <MenuBookIcon sx={{ marginRight: "10px" }} />
+            <Link to="/" style={{ color: "#fff", textDecoration: "none" }}>
+              BookBusters
+            </Link>
+            <SearchBar />
+          </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -130,28 +137,33 @@ const ResponsiveAppBar = () => {
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
+                vertical: "bottom",
+                horizontal: "left",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
+                vertical: "top",
+                horizontal: "left",
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' },
+                display: { xs: "block", md: "none" },
               }}
             >
               {/* key={`MenuItem-Desktop-NavBar--${index}`} */}
               {pages.map((page, index) => (
-                        <MenuItem key={`MenuItem-Desktop-NavBar--${index}`} onClick={handleCloseNavMenu} sx={page.sx}>
-                        <Link style={page.style} to={page.href} >
-                            <Typography sx={page.sx} textAlign="center">{page.label}</Typography>
-                        </Link>
-                        </MenuItem>
-
+                <MenuItem
+                  key={`MenuItem-Desktop-NavBar--${index}`}
+                  onClick={handleCloseNavMenu}
+                  sx={page.sx}
+                >
+                  <Link style={page.style} to={page.href}>
+                    <Typography sx={page.sx} textAlign="center">
+                      {page.label}
+                    </Typography>
+                  </Link>
+                </MenuItem>
               ))}
             </Menu>
           </Box>
@@ -159,18 +171,29 @@ const ResponsiveAppBar = () => {
             variant="h6"
             noWrap
             component="div"
-            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
           >
-          <MenuBookIcon sx={{marginRight: '10px'}}/>
-            <Link to="/" underline="none" style={{color:"#fff", textDecoration: 'none'}}>
+            <MenuBookIcon sx={{ marginRight: "10px" }} />
+            <Link
+              to="/"
+              underline="none"
+              style={{ color: "#fff", textDecoration: "none" }}
+            >
               BookBusters
             </Link>
+            <SearchBar />
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page, index) => (
-              <MenuItem key={`MenuItem-babybel-NavBar--${index}`} onClick={handleCloseNavMenu} sx={page.sx}>
+              <MenuItem
+                key={`MenuItem-babybel-NavBar--${index}`}
+                onClick={handleCloseNavMenu}
+                sx={page.sx}
+              >
                 <Link style={page.style} to={page.href}>
-                  <Typography sx={page.sx} textAlign="center">{page.label}</Typography>
+                  <Typography sx={page.sx} textAlign="center">
+                    {page.label}
+                  </Typography>
                 </Link>
               </MenuItem>
             ))}
@@ -183,25 +206,31 @@ const ResponsiveAppBar = () => {
               </IconButton>
             </Tooltip>
             <Menu
-              sx={{ mt: '45px'}}
+              sx={{ mt: "45px" }}
               id="menu-appbar"
               anchorEl={anchorElUser}
               anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
               {settings.map((setting, index) => (
-                <MenuItem key={`MenuItem-banana-NavBar--${index}`} onClick={handleCloseNavMenu} sx={setting.sx}>
-                  <Link style={setting.style} to={setting.href} >
-                    <Typography sx={setting.sx} textAlign="center">{setting.label}</Typography>
+                <MenuItem
+                  key={`MenuItem-banana-NavBar--${index}`}
+                  onClick={handleCloseNavMenu}
+                  sx={setting.sx}
+                >
+                  <Link style={setting.style} to={setting.href}>
+                    <Typography sx={setting.sx} textAlign="center">
+                      {setting.label}
+                    </Typography>
                   </Link>
                 </MenuItem>
               ))}
--- FRONT/bookbustersfront/src/pages/SignIn.js
+++ FRONT/bookbustersfront/src/pages/SignIn.js
@@ -1,49 +1,75 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
+import React, { useEffect, useRef, useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
 import Header from "../components/Header/Header";
-import LibrarySign from '../assets/img/library.jpg';
+import LibrarySign from "../assets/img/library.jpg";
 
+//* Import composant Link React-Router
+import { Link } from "react-router-dom";
+//* Import d'axios
+import axios from "axios";
 
-import { Link } from 'react-router-dom';
+async function getUser() {
+  try {
+    const response = await axios.post("http://localhost:5000/v1/login", {
+      login: "elodie.book@busters.fr",
+      password: "test",
+    });
+    console.log(response);
+  } catch (error) {
+    console.log("error");
+  }
+}
 
+// cet appel de fonction, doit être déclenchée quand user appuit sur se connecter
+getUser();
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link to="/" style={{color:"#000", textDecoration: 'underline'}}>
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link to="/" style={{ color: "#000", textDecoration: "underline" }}>
         BookBusters
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
 export default function SignInSide() {
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
   };
 
   return (
     <>
-    <Header />
-      <Grid container component="main" sx={{ height: '100vh' }}>
+      <Header />
+      <Grid container component="main" sx={{ height: "100vh" }}>
         <CssBaseline />
         <Grid
           item
@@ -52,11 +78,13 @@ export default function SignInSide() {
           md={7}
           sx={{
             backgroundImage: `url(${LibrarySign})`,
-            backgroundRepeat: 'no-repeat',
+            backgroundRepeat: "no-repeat",
             backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
           }}
         />
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
@@ -64,18 +92,23 @@ export default function SignInSide() {
             sx={{
               my: 8,
               mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
             }}
           >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
               <LockOutlinedIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
               Connexion
             </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
               <TextField
                 margin="normal"
                 required
@@ -110,12 +143,18 @@ export default function SignInSide() {
               </Button>
               <Grid container>
                 <Grid item xs>
-                  <Link to="/ForgotPassword" style={{color:"#000", textDecoration: 'underline'}}>
+                  <Link
+                    to="/ForgotPassword"
+                    style={{ color: "#000", textDecoration: "underline" }}
+                  >
                     Mot de passe oublié?
                   </Link>
                 </Grid>
                 <Grid item>
-                  <Link to="/SignUp" style={{color:"#000", textDecoration: 'underline'}}>
+                  <Link
+                    to="/SignUp"
+                    style={{ color: "#000", textDecoration: "underline" }}
+                  >
                     {"Pas encore inscrit ? Inscrivez-vous"}
                   </Link>
                 </Grid>
