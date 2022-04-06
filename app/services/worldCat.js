d3a6caf581356f86af5549034cb0b645cae0690b
JulienDupont11 <dupont.julien@gmail.com>
2022-04-06 16:03:01 +0200

    WorldCat route ok, need to be connected to the google one

--- app/services/worldCat.js
+++ app/services/worldCat.js
@@ -1,50 +1,59 @@
-//const debug = require('debug')('openLibraryAPI')
+/* eslint-disable no-useless-catch */
+// const debug = require('debug')('openLibraryAPI')
 
 const fetch = require('node-fetch');
+const xml2js = require('xml2js')
 
+const parser = new xml2js.Parser({ explicitArray: false, ignoreAttrs: false, mergeAttrs: true, explicitArray: false })
 /**
  * @typedef {object} BookCover
  * @property {string} cover - Book cover url
  */
 
 const worldCat = {
-    async findBookCoverByISBN(isbn) {
+    async findBookByISBN(isbn) {
         try {
             const url = `http://classify.oclc.org/classify2/Classify?isbn=${isbn}&summary=true`;
             const response = await fetch(url);
-            const json = await response.json();
-
-            debug(json);
-
+            const content = await response.text();
+            const data = await parser.parseStringPromise(content);
             let result = {};
-
-            // If no answer
-            if (Object.entries(json).length === 0) {
-                result = undefined;
-            }
-
-            // If at least one answer
-            if (Object.entries(json).length >= 1) {
-                debug('1 resultat');
-                const ISBNKey = `ISBN:${isbn}`;
-
-                debug(ISBNKey);
-
-                // If no cover found in result
-                if (!(json[ISBNKey].thumbnail_url)) {
-                    debug('pas de cover trouvée dans le résultat');
-                    result = undefined;
-                } else {
-                    debug('cover trouvée dans le résultat');
-                    result = {
-                        coverM: json[ISBNKey].thumbnail_url.split('-S').join('-M'),
-                        coverL: json[ISBNKey].thumbnail_url.split('-S').join('-L'),
-                    };
+            if (data.classify.works) {
+                const books = data.classify.works.work;
+                const APIIsbn = data.classify.input._;
+                console.log(APIIsbn);
+                const regex1 = new RegExp(/^97[8-9]\d{10}$/);
+                const regex2 = new RegExp(/^97[8-9]\d{7}$/);
+                let isbn13 = null;
+                let isbn10 = null;
+                if (regex1.test(APIIsbn)) {
+                    isbn13 = APIIsbn;
                 }
+                if (regex2.test(APIIsbn)) {
+                    isbn10 = APIIsbn;
+                }
+                console.log(isbn13);
+                console.log("books", books);
+
+                for (const book of books) {
+                    if (book.format == 'Book') {
+                        result = {
+                            isbn13: isbn13,
+                            isbn10: isbn10,
+                            title: book.title,
+                            author: book.author,
+                            publishedDate: book.lyr,
+                        };
+                        return result;
+                    }
+                };
             }
-            debug(result);
-            return result;
-        } catch (error) {
+            else {
+                result = undefined;
+                return result;
+            }
+        }
+        catch (error) {
             throw error;
         }
     },
