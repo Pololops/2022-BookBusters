7bcec28b66afdce4aea2c659b14eee62c7e37a37
ElodieFaivre <elodie.faivre3@gmail.com>
2022-04-07 12:42:50 +0200

    update getBookInformation controller to add relation and update controllers

--- BACK/app/middlewares/getBookInformation.js
+++ BACK/app/middlewares/getBookInformation.js
@@ -1,34 +1,49 @@
 const google = require('../services/google');
 const openLib = require('../services/openLibrary');
+const bookDataMapper = require('../models/book');
+const debug = require('debug')('getBookInfo');
 
 module.exports = {
     /**
      * Book middleware to get all books information.
      * @param {[Book]} books Array of book in BDD
      */
-    async getBookInformation(books) {
+    async getBookInformation(books, userId) {
         const googleQueries = [];
         const openLibraryQueries = [];
+        const userBookRelationQueries = [];
+
 
         books.forEach((book) => {
             if(book.isbn13){
                 googleQueries.push(google.findBookByISBN(book.isbn13));
                 openLibraryQueries.push(openLib.findBookCoverByISBN(book.isbn13));
+                if(userId){
+                    debug('ajout relation');
+                    userBookRelationQueries.push(bookDataMapper.findRelationBookUserWithISBN13(book.isbn13, userId))
+                }
             }
             else if(book.isbn10)
             {
                 googleQueries.push(google.findBookByISBN(book.isbn10));
                 openLibraryQueries.push(openLib.findBookCoverByISBN(book.isbn10));
+                if(userId){
+                    debug('ajout relation');
+                    userBookRelationQueries.push(bookDataMapper.findRelationBookUserWithISBN10(book.isbn10, userId))
+                }
             }
+
+
 
         });
 
         const googleResult = await Promise.all(googleQueries);
         const openLibResult = await Promise.all(openLibraryQueries);
+        const userBookRelationResult = await Promise.all(userBookRelationQueries);
 
         const newBooks = [];
         for (let i = 0; i < books.length; i += 1) {
-            newBooks.push({ ...books[i], ...googleResult[i], ...openLibResult[i] });
+            newBooks.push({ ...books[i], ...googleResult[i], ...openLibResult[i], ...userBookRelationResult[i] });
         }
 
         return newBooks;
--- BACK/app/routers/apiRouter.js
+++ BACK/app/routers/apiRouter.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 const controllerHandler = require('../middlewares/controllerWrapperAsync');
+const { verifyToken, verifyTokenWithoutError } = require('../middlewares/tokenVerifier');
+
 const apiController = require('../controllers/apiController');
 
 router
@@ -11,11 +13,12 @@ router
      * GET /v1/book/isbn/{isbn}
      * @summary Find one book with ISBN in google API
      * @tags SEARCH BOOK API
+     * @security BearerAuth
      * @param {number} isbn.path.required - book ISBN
      * @return {BookInfo} 200 - success response - application/json
      * @return  {ApiError} 404 - Book not found
      */
-    .get(controllerHandler(apiController.getBookByISBN));
+    .get(controllerHandler(verifyTokenWithoutError),controllerHandler(apiController.getBookByISBN));
 
 router
     .route('/book/cover/isbn/:isbn')
@@ -23,11 +26,12 @@ router
      * GET /v1/book/cover/isbn/{isbn}
      * @summary Find one book cover with ISBN in Open Library API
      * @tags SEARCH BOOK API
+     * @security BearerAuth
      * @param {number} isbn.path.required - book ISBN
      * @return {BookCover} 200 - success response - application/json
      * @return  {ApiError} 404 - Book not found
      */
-    .get(controllerHandler(apiController.getBookCoverByISBN));
+    .get(controllerHandler(verifyTokenWithoutError),controllerHandler(apiController.getBookCoverByISBN));
 
 router
     .route('/book/search')
@@ -35,9 +39,10 @@ router
      * GET /v1/book/search
      * @summary Find books by Keyword
      * @tags SEARCH BOOK API
+     * @security BearerAuth
      * @param {string} q.query
      * @return {[BookInfo]} 200 - success response - application/json
      */
-    .get(controllerHandler(apiController.getBookByKeyword));
+    .get(controllerHandler(verifyTokenWithoutError),controllerHandler(apiController.getBookByKeyword));
 
 module.exports = router;
