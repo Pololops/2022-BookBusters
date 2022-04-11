390cced267f5e77810eada91b2ad64804c01406c
YvanMackowiak <yvanmackowiak@gmail.com>
2022-04-11 15:43:32 +0200

    map des pin

--- FRONT/bookbustersfront/src/pages/AroundMe.js
+++ FRONT/bookbustersfront/src/pages/AroundMe.js
@@ -1,26 +1,23 @@
 //import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Buttons from "../components/Button/Button";
 import Header from "../components/Header/Header";
 
 import "../styles/AroundMe.scss";
 //import axios from "axios";
-import { mapAroundMe } from "../api/fetchApi";
+import { usersAroundMe } from "../api/fetchApi";
 
 const AroundMe = () => {
-  //const options = { location: "location: (48.5,2.3), radius : 200" };
-  /*  axios
-    .post("http://localhost:5000/v1/book/around-me", {
-      location: "(50.67987000000005,3.0685400000000413)",
-      radius: "200",
-    })
-    .then((response) => console.log(response))
-    .catch((error) => {
-      console.log(error);
-    });
-*/ mapAroundMe();
+  const [positionUser, setpositionUser] = useState([]);
+
+  useEffect(() => {
+    usersAroundMe(setpositionUser);
+  }, []);
+
+  const position = [50.67987000000005, 3.0685400000000413];
+
   return (
     <div>
       <Header />
@@ -31,9 +28,20 @@ const AroundMe = () => {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={[50.67987000000005, 3.0685400000000413]}>
+          {/*<Marker position={[50.67987000000005, 3.0685400000000413]}>
             <Popup>je suis une patate</Popup>
-          </Marker>
+          </Marker>*/}
+          {positionUser.map((user, index) => (
+            <Marker
+              key={index}
+              position={{
+                lat: user.loc.replace("(", " ").split(",")[0],
+                lng: user.loc.replace(")", " ").split(",")[1],
+              }}
+            >
+              <Popup>je suis une patate</Popup>
+            </Marker>
+          ))}
         </MapContainer>
       </div>
     </div>
