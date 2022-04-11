4882fc70e78554d6642c0f98dfc7dc82cfd61a75
Pololops <paul.rigaudeau@me.com>
2022-04-11 20:34:26 +0200

    add sql migration

--- BACK/app/controllers/bookController.js
+++ BACK/app/controllers/bookController.js
@@ -12,20 +12,33 @@ module.exports = {
      * @returns {string} Route API JSON response
      */
     async getAllInDonation(req, res) {
-        debug('GetAllInDonation')
-        let books = await bookDataMapper.findAllInDonation();
+        let connectedUserId;
+        if (!req.body.user) {
+            connectedUserId = 0;
+        } else {
+            connectedUserId = Number(req.body.user.userId);
+        }
+
+        let books = await bookDataMapper.findBooksInDonation(connectedUserId);
         books = await bookReformatter.reformat(books, req.body.user);
 
         return res.json(books);
     },
 
     async getOneBookById(req, res) {
+        let connectedUserId;
+        if (!req.body.user) {
+            connectedUserId = 0;
+        } else {
+            connectedUserId = Number(req.body.user.userId);
+        }
+
         const bookId = req.params.id;
-        let book = await bookDataMapper.findOneBookById(bookId);
+        let book = await bookDataMapper.findBooks(connectedUserId, `{${bookId}}`, '{}', '{}', 1, 0);
         if (!book) {
             throw new ApiError('Book not found', { statusCode: 404 });
         }
-        book = await bookReformatter.reformat([book], req.body.user);
+        book = await bookReformatter.reformat(book, req.body.user);
 
         return res.json(book);
     },
@@ -60,40 +73,40 @@ module.exports = {
 
     async getBooksWithIds(req, res) {
         debug('Req.query.books = ', req.query.books);
-        let bookIds = req.query.books
-        bookIds = bookIds.substr(1).substr(0, bookIds.length - 2).split(',');
+        let bookIds = req.query.books;
+        bookIds = bookIds
+            .substr(1)
+            .substr(0, bookIds.length - 2)
+            .split(',');
         debug('après traitement', bookIds);
 
         const promiseToSolve = [];
-        bookIds.forEach(element => {
+        bookIds.forEach((element) => {
             promiseToSolve.push(bookDataMapper.findOneBookById(Number(element)));
         });
 
-
         //TODO : question : what happened si une promesse échoue ??
-        debug('Je lance les promesses pour trouver les livres')
+        debug('Je lance les promesses pour trouver les livres');
         let books = await Promise.all(promiseToSolve);
         debug('Les livres trouvés sont', books);
         //Without books undefined
-        let newBooks=[];
-        books.forEach(book=>{
-            if(book) {
+        let newBooks = [];
+        books.forEach((book) => {
+            if (book) {
                 newBooks.push(book);
-        }
+            }
         });
-        books=newBooks;
-
+        books = newBooks;
 
         debug('Les livres trouvés sans les undefined', books);
 
         debug('Je complete les infos avec API');
 
-        debug('user connecté')
+        debug('user connecté');
         books = await bookReformatter.reformat(books, req.body.user);
 
         debug('Livres complets', books);
 
-       return res.json(books);
-
-    }
+        return res.json(books);
+    },
 };
