5e61ef4a66e44710a9a207c3e09108508bde9a14
YvanMackowiak <yvanmackowiak@gmail.com>
2022-04-11 10:53:40 +0200

    ajout des user dans la fiche des book

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,11 +1,10 @@
 import { Box } from "@mui/system";
-import { Button, ButtonGroup, Container } from '@mui/material';
+import { Button, ButtonGroup, Container } from "@mui/material";
 
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 import Book from "../Book/Book";
-
 
 // C'est ici que nous allons faire notre map pour afficher plusieurs livres.
 
@@ -29,9 +28,9 @@ function Books() {
         {/*data.map((livre, index) => (
           <Book key={index} livre={livre} />
         ))*/}
-        {data.map((livre) =>
-          livre.user.map((user, index) => <Book key={`je-suis-unique-${index}`} livre={livre} user={user} />)
-        )}
+        {data.map((livre, index) => (
+          <Book key={`je-suis-unique-${index}`} livre={livre} users={livre.user} />
+        ))}
       </Box>
     </>
   );
