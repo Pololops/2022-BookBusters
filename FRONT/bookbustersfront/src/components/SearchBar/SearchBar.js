c87a43f5d36311529062e4782b34654f2226de2f
Pierre-Nicolas <pnpvdc@gmail.com>
2022-04-12 11:34:36 +0200

    Route get writing

--- FRONT/bookbustersfront/src/components/SearchBar/SearchBar.js
+++ FRONT/bookbustersfront/src/components/SearchBar/SearchBar.js
@@ -34,7 +34,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
+    // transition: theme.transitions.create("width"),
     width: "100%",
     [theme.breakpoints.up("sm")]: {
       width: "12ch",
