6ffcdced4e4f739b268ad1459857ea4c74f45d93
Pololops <paul.rigaudeau@me.com>
2022-04-14 22:31:09 +0200

    best resoltion for covers and default cover

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,5 +4,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://bookbusters.herokuapp.com/",
+    // baseURL: 'https://bookbusters.herokuapp.com/',
+    baseURL: 'http://localhost:5000/',
 });
