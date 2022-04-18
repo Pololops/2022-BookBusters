ce105f34bd7f633ebd2ec605d32a7ba84ab2373c
Pololops <paul.rigaudeau@me.com>
2022-04-18 02:17:20 +0200

    increase configuration of the isbn scanner

--- FRONT/bookbustersfront/src/pages/Scan.js
+++ FRONT/bookbustersfront/src/pages/Scan.js
@@ -1,73 +1,35 @@
 import { Box } from '@mui/system';
 
-import '../styles/Scan.scss';
-
-import React, { useEffect, useContext } from 'react';
-import { Html5QrcodeScanner } from 'html5-qrcode';
+import React from 'react';
 import Header from '../components/Header/Header';
 
-import { searchBookByISBN } from '../api/fetchApi';
+import Scanner from '../components/Scan/Scan';
+
 import BookDetailModal from '../components/BookDetailModal/BookDetailModal';
 
-import bookContext from '../contexts/BookContext';
+import SearchBar from '../components/SearchBar/SearchBar';
 
-const Scanner = () => {
-    const { setOpenedBook } = useContext(bookContext);
-
-    useEffect(() => {
-        async function onScanSuccess(decodedText, decodedResult) {
-            const isbn = decodedText;
-            console.log('error : ', isbn);
-            if (isbn.length === 13) {
-                console.log('ok : ', isbn);
-                html5Qrcode.clear();
-
-                const response = await searchBookByISBN(isbn);
-
-                setOpenedBook(response.data);
-            }
-        }
-
-        function onScanError(qrCodeError) {
-            // This callback would be called in case of qr code scan error or setup error.
-            // You can avoid this callback completely, as it can be very verbose in nature.
-        }
-
-
-        const config = {
-            facingMode: 'environment',
-            fps: 60,
-            qrbox: { width: 250, height: 140 },
-            disableFlip: false,
-            rememberLastUsedCamera: true,
-            // Only support camera scan type.
-            // supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA],
-            // Restrict barcode format to EAN_13
-            // formatsToSupport: [Html5QrcodeSupportedFormats.EAN_13],
-            // experimentalFeatures: {
-            //     useBarCodeDetectorIfSupported: true,
-            // },
-        };
-
-        const html5Qrcode = new Html5QrcodeScanner(
-            'reader',
-            config,
-            false,
-        );
-        html5Qrcode.render(onScanSuccess, onScanError);
-    });
-
+const Scan = () => {
     return (
         <>
             <Header />
-            <Box>
-                <div id='scanner'>
-                    <div id='reader'></div>
-                </div>
-                <BookDetailModal />
+            <Box
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    flexFlow: 'column wrap',
+                    textAlign: 'center',
+                    mt: 4,
+                }}
+            >
+                <Scanner />
             </Box>
+            <Box>
+                <SearchBar />
+            </Box>
+            <BookDetailModal />
         </>
     );
 };
 
-export default Scanner;
+export default Scan;
