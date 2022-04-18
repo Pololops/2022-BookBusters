d3b1795bb8eb129fdc5e24afa8c7be9d4ff37ee4
Julien <dupont.julien@gmail.com>
2022-04-18 19:00:32 +0200

    adding of the donationrenewing page

---
+++ src/pages/DonationRenewing.js
@@ -0,0 +1,11 @@
+import Header from "../components/Header/Header";
+
+const DonationRenewing = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+};
+
+export default DonationRenewing;
