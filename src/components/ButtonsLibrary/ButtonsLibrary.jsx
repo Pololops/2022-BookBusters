90d55d358a9651406ca8c5fb97cfc9e857e81500
Pololops <paul.rigaudeau@me.com>
2022-04-20 20:38:50 +0200

    add somme css to lists and spinner

--- src/components/ButtonsLibrary/ButtonsLibrary.jsx
+++ src/components/ButtonsLibrary/ButtonsLibrary.jsx
@@ -1,24 +1,17 @@
-import { Container, ButtonGroup, Button } from "@mui/material";
+import { Container, ButtonGroup } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 import LinkButton from "../LinkButton/LinkButton";
 
 const ButtonsLibrary = () => {
   return (
-    <div>
-      <Container
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <ButtonGroup>
+      <Container sx={{ margin: "10px 0 30px 0", maxWidth: "100% !important", width: "100%" }}>
+        <ButtonGroup sx={{ gap: { xs: "1px", md: "25px" }, width: "100%", justifyContent: "center",}}>
           <LinkButton to="/Library" text={"Bibliothèque"} />
           <LinkButton to="/Favorites" text={"Favoris"} />
           <LinkButton to="/MyAlerts" text={"Alertes"} />
         </ButtonGroup>
       </Container>
-    </div>
   );
 };
 
