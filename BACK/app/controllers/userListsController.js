556fa1bc96c54cc5dd703b1fdd3e2acc23671ba3
Julien <dupont.julien@gmail.com>
2022-04-21 11:01:21 +0200

    ready to merge

--- BACK/app/controllers/userListsController.js
+++ BACK/app/controllers/userListsController.js
@@ -103,7 +103,6 @@ module.exports = {
         };
         const book = await bookDataMapper.findOneBookById(bookId1);
         return res.json({ book, association: updatedBook });
-        
     },
 
 };
