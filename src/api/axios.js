b51c1692d2d4a7ee8c8b4099461e047826f0fe8e
Pololops <paul.rigaudeau@me.com>
2022-04-21 18:21:16 +0200

    use localhost to fetch bbAPI

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,6 +4,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://bookbusters-server.herokuapp.com/',
-    // baseURL: 'http://localhost:5000/',
+    // baseURL: 'https://bookbusters-server.herokuapp.com/',
+    baseURL: 'http://localhost:5000/',
 });
