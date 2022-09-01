967e1876b31ec6fe9bbcad81fbb38511397c06e3
Pololops <paul.rigaudeau@me.com>
2022-09-01 09:29:46 +0200

    readd sqitch migration missed files and change the fetch url in frontend to target the aws server backend

--- FRONT/bookbustersfront/src/api/axios.js
+++ FRONT/bookbustersfront/src/api/axios.js
@@ -4,5 +4,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: (process.env.NODE_ENV === 'production') ? 'https://bookbusters-server.herokuapp.com/' : 'http://localhost:5000/',
+	baseURL:
+		process.env.NODE_ENV === 'production'
+			? 'http://bookbustersapi-env.eba-tif7y2yb.eu-west-3.elasticbeanstalk.com/'
+			: 'http://localhost:5000/',
 });
