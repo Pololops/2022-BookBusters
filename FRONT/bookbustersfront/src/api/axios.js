55138ed3a05ee380272f68a045c310c1f7fd7518
Pololops <paul.rigaudeau@me.com>
2022-09-04 21:42:52 +0200

    change axios url git push

--- FRONT/bookbustersfront/src/api/axios.js
+++ FRONT/bookbustersfront/src/api/axios.js
@@ -4,5 +4,5 @@
 import axios from "axios";
 
 export default axios.create({
-	baseURL: 'http://bookbustersapi-env.eba-tif7y2yb.eu-west-3.elasticbeanstalk.com/'
+	baseURL: 'https://bookbusters-api.eu-west-3.elasticbeanstalk.com/',
 });
