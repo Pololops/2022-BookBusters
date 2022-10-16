a859174ed877ae445e298ac34f366a66b32be1ce
Pololops <paul.rigaudeau@me.com>
2022-10-17 00:20:17 +0200

    change baseURL to use an environment variable

--- FRONT/bookbustersfront/src/api/axios.js
+++ FRONT/bookbustersfront/src/api/axios.js
@@ -4,8 +4,5 @@
 import axios from "axios";
 
 export default axios.create({
-	baseURL:
-		process.env.NODE_ENV === 'production'
-			? 'http://bookbustersapi-env.eba-tif7y2yb.eu-west-3.elasticbeanstalk.com/'
-			: 'http://localhost:5000/',
+	baseURL: process.env.REACT_APP_API_URL,
 });
